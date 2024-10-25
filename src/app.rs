047dbc30fe124b4c2059b868c773486f3c0fa20e
Niklas Nordkamp <nnord@d-velop.de>
2024-10-25 16:58:39 +0200

    selectable entries

--- src/app.rs
+++ src/app.rs
@@ -1,8 +1,8 @@
-use std::{cmp::Ordering, fs, io, path::Path};
+use std::{cmp::Ordering, collections::HashSet, fs, io, ops::Index, path::Path};
 
 use chrono::{DateTime, Utc};
 use egui::{Key, Label, Modifiers, PointerButton, Sense, TextEdit, Widget};
-use egui_dock::{DockArea, DockState, NodeIndex, Style, SurfaceIndex};
+use egui_dock::{DockArea, DockState, NodeIndex, Style, SurfaceIndex, TabIndex};
 use egui_extras::{Column, TableBuilder};
 
 #[derive(serde::Deserialize, serde::Serialize)]
@@ -20,6 +20,7 @@ pub struct App {
     pub data: AppData,
     #[serde(skip)]
     pub tabs: DockState<Tab>,
+    pub latest_tab_id: u64,
 }
 
 impl Default for App {
@@ -27,6 +28,7 @@ impl Default for App {
         Self {
             data: Default::default(),
             tabs: DockState::new(vec![]),
+            latest_tab_id: 0,
         }
     }
 }
@@ -41,7 +43,12 @@ impl App {
 
         app.tabs = DockState::new(vec![Tab::new2(
             app.data.favorites.first().unwrap_or(&"/".to_string()),
+            egui::Id::new(app.latest_tab_id),
         )]);
+        app.latest_tab_id += 1;
+        //app.tabs.set_active_tab((SurfaceIndex(0), NodeIndex(0), TabIndex(0)));
+        app.tabs
+            .set_focused_node_and_surface((SurfaceIndex(0), NodeIndex(0)));
         return app;
     }
 }
@@ -52,46 +59,11 @@ impl eframe::App for App {
     }
 
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        egui::CentralPanel::default().show(ctx, |ui| {
-            DockArea::new(&mut self.tabs)
-                .show_add_buttons(true)
-                .style({
-                    let mut style = Style::from_egui(ctx.style().as_ref());
-                    style.tab_bar.fill_tab_bar = true;
-                    style
-                })
-                .show(ctx, &mut self.data);
-
-            self.data.added_nodes.drain(..).for_each(|(surface, node)| {
-                self.tabs.set_focused_node_and_surface((surface, node));
-                self.tabs.push_to_focused_leaf(Tab::new2(
-                    self.data.favorites.first().unwrap_or(&"/".to_string()),
-                ));
-            });
-        });
-    }
-}
-
-impl egui_dock::TabViewer for AppData {
-    type Tab = Tab;
-
-    fn title(&mut self, tab: &mut Self::Tab) -> egui::WidgetText {
-        match &tab.info {
-            Ok(info) => info.file_name.clone().into(),
-            Err(_) => "error".into(),
-        }
-    }
-
-    fn on_add(&mut self, surface: SurfaceIndex, node: NodeIndex) {
-        self.added_nodes.push((surface, node));
-    }
-
-    fn ui(&mut self, ui: &mut egui::Ui, tab: &mut Self::Tab) {
-        egui::SidePanel::left("favorites").show_inside(ui, |ui| {
+        egui::SidePanel::left("favorites_tab").show(ctx, |ui| {
             ui.vertical(|ui| {
                 ui.heading("favorites");
                 let mut to_remove = None;
-                for (i,favorite) in self.favorites.iter().enumerate() {
+                for (i, favorite) in self.data.favorites.iter().enumerate() {
                     let path = Path::new(favorite);
                     let file_name = path
                         .file_name()
@@ -102,7 +74,10 @@ impl egui_dock::TabViewer for AppData {
                         .selectable(false)
                         .ui(ui);
                     if resp.clicked() {
-                        tab.new(favorite);
+                        let tab = self.tabs.find_active_focused();
+                        if let Some((rect, tab)) = tab {
+                            tab.new(favorite);
+                        }
                     }
                     resp.context_menu(|ui| {
                         if Label::new("x")
@@ -117,15 +92,56 @@ impl egui_dock::TabViewer for AppData {
                     });
                 }
                 if let Some(to_remove) = to_remove {
-                    self.favorites.remove(to_remove);
+                    self.data.favorites.remove(to_remove);
                 }
             });
         });
+        egui::CentralPanel::default().show(ctx, |ui| {
+            DockArea::new(&mut self.tabs)
+                .show_add_buttons(true)
+                .style({
+                    let mut style = Style::from_egui(ctx.style().as_ref());
+                    style.tab_bar.fill_tab_bar = true;
+                    style
+                })
+                .show(ctx, &mut self.data);
+
+            self.latest_tab_id += 1;
+            self.data.added_nodes.drain(..).for_each(|(surface, node)| {
+                self.tabs.set_focused_node_and_surface((surface, node));
+                self.tabs.push_to_focused_leaf(Tab::new2(
+                    self.data.favorites.first().unwrap_or(&"/".to_string()),
+                    egui::Id::new(self.latest_tab_id),
+                ));
+            });
+        });
+    }
+}
+
+impl egui_dock::TabViewer for AppData {
+    type Tab = Tab;
+
+    fn title(&mut self, tab: &mut Self::Tab) -> egui::WidgetText {
+        match &tab.info {
+            Ok(info) => info.path.clone().into(),
+            Err(_) => "invalid path".into(),
+        }
+    }
+
+    fn id(&mut self, tab: &mut Self::Tab) -> egui::Id {
+        tab.id
+    }
+
+    fn on_add(&mut self, surface: SurfaceIndex, node: NodeIndex) {
+        self.added_nodes.push((surface, node));
+    }
+
+    fn ui(&mut self, ui: &mut egui::Ui, tab: &mut Self::Tab) {
         if ui.input(|i| i.pointer.button_clicked(PointerButton::Extra1)) {
             // previous
             if !tab.previous_paths.is_empty() {
                 let last = tab.previous_paths.remove(tab.previous_paths.len() - 1);
-                let mut new = Tab::new2(last.clone());
+                let mut new = Tab::new2(last.clone(), tab.id);
                 new.previous_paths.append(&mut tab.previous_paths);
                 new.previous_paths2.append(&mut tab.previous_paths2);
                 new.previous_paths2.push(last);
@@ -151,26 +167,46 @@ impl egui_dock::TabViewer for AppData {
             if ui.button("⟳").clicked() {
                 tab.new(tab.path.clone());
             }
+            let search_width = 150.0;
             let resp = TextEdit::singleline(&mut tab.path)
-                .desired_width(ui.available_width())
+                .desired_width(ui.available_width() - search_width)
                 .return_key(Some(egui::KeyboardShortcut::new(
                     Modifiers::NONE,
                     Key::Enter,
                 )))
+                .cursor_at_end(true)
                 .show(ui);
 
             if resp.response.lost_focus() && ui.input(|i| i.key_pressed(egui::Key::Enter)) {
                 tab.new(tab.path.clone());
             }
+
+            TextEdit::singleline(&mut tab.search)
+                .return_key(Some(egui::KeyboardShortcut::new(
+                    Modifiers::NONE,
+                    Key::Enter,
+                )))
+                .hint_text("search")
+                .cursor_at_end(true)
+                .desired_width(search_width - 50.)
+                .show(ui);
+
+            if !tab.search.is_empty() {
+                if ui.button("X").clicked() {
+                    tab.search = "".into();
+                }
+            }
         });
 
-        if let Ok(entries) = &tab.entries {
+        if let Ok(entries) = &mut tab.entries {
             let mut new_path = None;
+
+            let ctx = ui.ctx().clone();
 
             TableBuilder::new(ui)
                 .column(Column::remainder())
-                .column(Column::remainder())
-                .column(Column::auto())
+                .column(Column::auto().at_least(160.))
+                .column(Column::auto().at_least(60.))
                 .sense(egui::Sense::click())
                 .header(20.0, |mut header| {
                     header.col(|ui| {
@@ -184,58 +220,102 @@ impl egui_dock::TabViewer for AppData {
                     });
                 })
                 .body(|mut body| {
-                    for entry in entries.iter() {
-                        body.row(18.0, |mut row| {
-                            row.col(|ui| {
-                                //ui.label(entry.file_name.to_string());
-                                Label::new(entry.file_name.to_string())
-                                    .selectable(false)
-                                    .ui(ui);
-                            });
-                            row.col(|ui| {
-                                ui.label(entry.modified.format("%d/%m/%Y %H:%M").to_string());
-                            });
-                            row.col(|ui| {
-                                if entry.file_type.is_file() {
-                                    ui.label(bytes_to_human_readable(entry.len));
-                                }
-                            });
+                    for (i, entry) in entries.iter_mut().enumerate() {
+                        if tab.search.is_empty()
+                            || entry
+                                .file_name
+                                .to_lowercase()
+                                .contains(&tab.search.to_lowercase())
+                        {
+                            body.row(18.0, |mut row| {
+                                row.set_selected(tab.selected_entries.contains(&i));
+                                row.col(|ui| {
+                                    let mut text: egui::RichText =
+                                        entry.file_name.to_string().into();
+                                    if entry.file_type.is_dir() {
+                                        text = text.strong();
+                                    }
+                                    Label::new(text).selectable(false).ui(ui);
+                                });
+                                row.col(|ui| {
+                                    ui.label(entry.modified.format("%d/%m/%Y %H:%M").to_string());
+                                });
+                                row.col(|ui| {
+                                    if entry.file_type.is_file() {
+                                        ui.label(bytes_to_human_readable(entry.len));
+                                    }
+                                });
 
-                            let resp = row.response();
-                            if resp.double_clicked() && entry.file_type.is_dir() {
-                                new_path = Some(entry.path.clone());
-                            }
+                                let resp = row.response();
+                                if resp.double_clicked() && entry.file_type.is_dir() {
+                                    new_path = Some(entry.path.clone());
+                                }
 
-                            resp.context_menu(|ui| {
-                                if ui.button("vscode").clicked() {
-                                    std::process::Command::new("code")
-                                        .arg(entry.path.clone())
-                                        .status();
-                                    ui.close_menu();
+                                let command = ctx.input(|i| i.modifiers.command);
+                                let shift = ctx.input(|i| i.modifiers.shift);
+                                if resp.clicked() {
+                                    
+                                    if shift {
+                                        if let Some(first) = tab.last_clicked_entry{
+                                            if first >= i {
+                                                for x in i..first {
+                                                    tab.selected_entries.insert(x);
+                                                }
+                                            } else {
+                                                for x in first+1..=i {
+                                                    tab.selected_entries.insert(x);
+                                                }
+                                            }
+                                            
+                                        }
+                                    } else if command {
+                                        if tab.selected_entries.contains(&i) {
+                                            tab.selected_entries.remove(&i);
+                                        } else {
+                                            tab.selected_entries.insert(i);
+                                        }
+                                    } else {
+                                        if tab.selected_entries.contains(&i) {
+                                            tab.selected_entries.clear();
+                                        } else {
+                                            tab.selected_entries.clear();
+                                            tab.selected_entries.insert(i);
+                                        }
+                                    }
+                                    tab.last_clicked_entry = Some(i);
                                 }
-                                if ui.button("terminal").clicked() {
-                                    std::process::Command::new("open")
-                                        .arg("-a")
-                                        .arg("Terminal")
-                                        .arg(entry.path.clone())
-                                        .status();
-                                    ui.close_menu();
-                                }
-                                if ui.button("make executable").clicked() {
-                                    std::process::Command::new("chmod")
-                                        .arg("755")
-                                        .arg(entry.path.clone())
-                                        .status();
-                                    ui.close_menu();
-                                }
-                                if ui.button("zed").clicked() {
-                                    std::process::Command::new("zed")
-                                        .arg(entry.path.clone())
-                                        .status();
-                                    ui.close_menu();
-                                }
+
+                                resp.context_menu(|ui| {
+                                    if ui.button("vscode").clicked() {
+                                        std::process::Command::new("code")
+                                            .arg(entry.path.clone())
+                                            .status();
+                                        ui.close_menu();
+                                    }
+                                    if ui.button("terminal").clicked() {
+                                        std::process::Command::new("open")
+                                            .arg("-a")
+                                            .arg("Terminal")
+                                            .arg(entry.path.clone())
+                                            .status();
+                                        ui.close_menu();
+                                    }
+                                    if ui.button("make executable").clicked() {
+                                        std::process::Command::new("chmod")
+                                            .arg("755")
+                                            .arg(entry.path.clone())
+                                            .status();
+                                        ui.close_menu();
+                                    }
+                                    if ui.button("zed").clicked() {
+                                        std::process::Command::new("zed")
+                                            .arg(entry.path.clone())
+                                            .status();
+                                        ui.close_menu();
+                                    }
+                                });
                             });
-                        });
+                        }
                     }
                 });
 
@@ -246,25 +326,34 @@ impl egui_dock::TabViewer for AppData {
     }
 }
 
+#[derive(Debug)]
 pub struct Tab {
+    pub id: egui::Id,
     pub path: String,
+    pub search: String,
     pub info: io::Result<FileEntry>,
     pub entries: Result<Vec<FileEntry>, io::Error>,
+    pub selected_entries: HashSet<usize>,
+    pub last_clicked_entry: Option<usize>,
     pub previous_paths: Vec<String>,
     pub previous_paths2: Vec<String>,
 }
 
 impl Tab {
-    fn new2(path: impl Into<String>) -> Self {
+    fn new2(path: impl Into<String>, id: egui::Id) -> Self {
         let path = path.into();
         let info = get_meta(&path);
         let entries = get_entries(&path);
         return Self {
+            id,
             path,
+            search: "".into(),
             info,
             entries,
             previous_paths: vec![],
             previous_paths2: vec![],
+            selected_entries: Default::default(),
+            last_clicked_entry: None,
         };
     }
 
@@ -276,7 +365,7 @@ impl Tab {
             }
         }
 
-        let mut new = Self::new2(path);
+        let mut new = Self::new2(path, self.id);
         new.previous_paths.append(&mut self.previous_paths);
         new.previous_paths.push(self.path.clone());
         *self = new;
@@ -305,7 +394,6 @@ fn get_meta(path: &str) -> io::Result<FileEntry> {
             .to_str()
             .unwrap_or_default()
             .to_string(),
-        selected: false,
     });
 }
 
@@ -330,7 +418,6 @@ fn get_entries(path: &str) -> io::Result<Vec<FileEntry>> {
             accessed,
             path,
             file_name,
-            selected: false,
         });
     }
     files.sort_by(|a, b| {
@@ -362,6 +449,7 @@ fn bytes_to_human_readable(bytes: u64) -> String {
     }
 }
 
+#[derive(Debug)]
 pub struct FileEntry {
     pub len: u64,
     pub file_type: fs::FileType,
@@ -370,5 +458,4 @@ pub struct FileEntry {
     pub accessed: DateTime<Utc>,
     pub path: String,
     pub file_name: String,
-    pub selected: bool,
 }
